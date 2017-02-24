dbd2c295acc54609e4b5f62b88ccd2e1848b61f2
Atma Mani <amani@esri.com>
2017-02-24 13:59:19 -0800

    split cloud formation bat into two, other bug fixes

--- samples/03_org_administrators/AdminCreatePortal/cloud_formation_b.bat
+++ samples/03_org_administrators/AdminCreatePortal/cloud_formation_b.bat
@@ -12,31 +12,26 @@ REM Call this script with portal URL, admin username, admin password
 REM Prints CF_success at the end
 
 ECHO off
-REM 1. Download miniconda to active dir
-ECHO Downloading latest miniconda
-powershell -Command "Invoke-WebRequest https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -OutFile miniconda.exe"
-
-REM 2. Install miniconda
-ECHO Install miniconda in user profile. Not registering Python
-start /wait "" miniconda.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%UserProfile%\miniconda3
 
 REM 3a. Create new env called 'cloud_formation
 ECHO Creating new environment
+call deactivate
+conda env remove -y --name cloud_formation
 conda create --name cloud_formation python=3.6
-conda activate cloud_formation
+call conda activate cloud_formation
 
 REM 3b. Install arcgis
 ECHO Installing arcgis package
-conda isntall -c esri arcgis
+call conda isntall -c esri arcgis
 
 REM 4. Run clean up script
-python.exe cleanup.py %1 %2 %3
+python.exe cleanup.py %1 -u %2 -p %3
 
 REM 5. Run create_groups script
-python.exe create_groups.py %1 %2 %3
+REM python.exe create_groups.py %1 -u %2 -p %3
 
 REM 6. Run create_users script
-python.exe create_users.py %1 %2 %3
+REM python.exe create_users.py %1 -u %2 -p %3
 
 REM 7. Run publish_content script
-python.exe publish_content.py %1 %2 %3
+REM python.exe publish_content.py %1 -u %2 -p %3
