defc0137a4044a02c05eaaf73f1b7a174389c817
Akhil Negi <akh10431@esri.com>
2020-06-29 12:55:03 -0700

    updating --skipautouninstaller flg

--- misc/tools/doccano_deployment/uninstall.ps1
+++ misc/tools/doccano_deployment/uninstall.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
-"y"|choco uninstall git.install -n --skip-autouninstaller
-choco uninstall python -y -n --skip-autouninstal
-"y"|choco uninstall nodejs --version=13.11.0 -n --skip-autouninstaller
-"y"|choco uninstall yarn -n --skip-autouninstaller
-"y"|choco uninstall nssm -n --skip-autouninstaller
+"y"|choco uninstall git.install -n --skipautouninstaller 
+choco uninstall python -y -n --skipautouninstaller
+"y"|choco uninstall nodejs --version=13.11.0 -n --skipautouninstaller
+"y"|choco uninstall yarn -n --skipautouninstaller
+"y"|choco uninstall nssm -n --skipautouninstaller
