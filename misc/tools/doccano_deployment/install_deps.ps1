e475a1059d276d0b1df4ba99ef97983a8453a01a
Akhil Negi <akh10431@esri.com>
2020-06-30 03:06:01 -0700

    updating python version to 3.7.2 and creating the virtual env for docanno at root of C directory to avoid long path problem.

--- misc/tools/doccano_deployment/install_deps.ps1
+++ misc/tools/doccano_deployment/install_deps.ps1
@@ -1,14 +1,14 @@
 
 $git_installed = cver all -lo | Select-string -Pattern "^git"
 if ($git_installed.length -eq 0 ) {
-    choco install -y git.install
+   choco install -y git.install
     }
 else {
     echo "Skipping git installation"
     }
 $python_installed = cver all -lo | Select-string -Pattern "^python"
 if ($python_installed.length -eq 0 ) {
-    choco install -y python --version=3.6.8
+    choco install -y python --version=3.7.2
     }
 else {
     echo "Skipping python installation"
--- misc/tools/doccano_deployment/uninstall.ps1
+++ misc/tools/doccano_deployment/uninstall.ps1
@@ -1,6 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
-"y"|choco uninstall git.install -n --skipautouninstaller 
-choco uninstall python -y -n --skipautouninstaller
+"y"|choco uninstall git.install -n --skipautouninstaller
+"y"|choco uninstall python --version=3.7.2 -n --skipautouninstaller
 "y"|choco uninstall nodejs --version=13.11.0 -n --skipautouninstaller
-"y"|choco uninstall yarn -n --skipautouninstaller
 "y"|choco uninstall nssm -n --skipautouninstaller
